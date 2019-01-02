3559ff9f269fc58503300f451fd48f81b61e5753
amosar <maxi.cass11@gmail.com>
2019-01-02 21:02:09 +0100

    Fix a bug with the latest version of WorldGuard

--- src/com/trafalcraft/antiRedstoneClock/util/WorldGuard/WorldGuard_7.java
+++ src/com/trafalcraft/antiRedstoneClock/util/WorldGuard/WorldGuard_7.java
@@ -10,7 +10,7 @@ class WorldGuard_7 {
     static RegionManager getRegionManager(WorldGuardPlugin worldGuard, World world) {
         BukkitWorldGuardPlatform wgPlatform = (BukkitWorldGuardPlatform)
                 WorldGuard.getInstance().getPlatform();
-        RegionManager regionManager = wgPlatform.getRegionContainer().get(wgPlatform.getWorldByName(world.getName()));
-        return regionManager;
+        com.sk89q.worldedit.world.World worldEditWorld = wgPlatform.getMatcher().getWorldByName(world.getName());
+        return wgPlatform.getRegionContainer().get(worldEditWorld);
     }
 }

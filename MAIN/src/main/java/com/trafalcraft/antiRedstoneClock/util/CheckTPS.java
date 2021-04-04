1ae08581d7a313e60b767e78e890febbeb99477b
Amosar <maxi.cass11@gmail.com>
2021-04-04 14:00:51 +0200

    Improve checkTPS to work as documented

--- MAIN/src/main/java/com/trafalcraft/antiRedstoneClock/util/CheckTPS.java
+++ MAIN/src/main/java/com/trafalcraft/antiRedstoneClock/util/CheckTPS.java
@@ -23,27 +23,22 @@ public class CheckTPS {
         if (repeatingTaskID != -1) {
             Bukkit.getScheduler().cancelTask(repeatingTaskID);
         }
-        if (minimumTPS != -1 && maximumTPS != -1) {
+        if (minimumTPS <= 0 && maximumTPS <= 0) {
             repeatingTaskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getInstance(), () -> {
-                if (minimumTPS <= 0) {
-                    Bukkit.getScheduler().cancelTask(repeatingTaskID);
-                    repeatingTaskID = -1;
-                } else {
-                    long now = System.currentTimeMillis();
-                    long timeSpent = (now - lastPoll) / 1000;
-                    if (timeSpent == 0){
-                        timeSpent = 1;
-                    }
-                    tps = (20*interval / timeSpent);
-                    if (minimumTPS < 0) {
-                        tpsIsOK = tps <= maximumTPS;
-                    } else if (maximumTPS < 0) {
-                        tpsIsOK = tps >= minimumTPS;
-                    } else {
-                        tpsIsOK = tps >= minimumTPS && tps <= maximumTPS;
-                    }
-                    lastPoll = now;
+                long now = System.currentTimeMillis();
+                long timeSpent = (now - lastPoll) / 1000;
+                if (timeSpent == 0){
+                    timeSpent = 1;
                 }
+                tps = (20*interval / timeSpent);
+                if (minimumTPS < 0) {
+                    tpsIsOK = tps <= maximumTPS;
+                } else if (maximumTPS < 0) {
+                    tpsIsOK = tps >= minimumTPS;
+                } else {
+                    tpsIsOK = tps >= minimumTPS && tps <= maximumTPS;
+                }
+                lastPoll = now;
             }, 0, 20*interval);
         }
     }
